09633c1cdf3e0f0098f263e541a97c35369c8549
yannicklin <yannicklin.web@gmail.com>
2017-09-15 12:33:30 +1000

    update favicon and footer with link back to main website and also the year automatically update.

--- foot.php
+++ foot.php
@@ -1,21 +1,1 @@
-            <!-- FOOTER -->
-            <footer>
-                <p class="pull-right"><a href="#">Back to top</a></p>
-                <p>&copy; 2014 Yannick &middot; No Privacy & Terms</p>
-            </footer>
-        </div>
-        <!-- /.container -->
-
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="js/bootstrap.min.js"></script>
-
-        <!-- Additional tags here -->
-        <?php if (function_exists('customPageFooter')){
-            customPageFooter();
-        }
-        ?>
-
-    </body>
-</html>
+            <!-- FOOTER -->            <footer>                <p class="pull-right"><a href="#">Back to top</a></p>                <p>&copy; 2014 - <?php echo date("Y"); ?> <a href="https://yannicklin.net">Yannick</a> &middot; No Privacy & Terms</p>            </footer>        </div>        <!-- /.container -->        <!-- Bootstrap core JavaScript        ================================================== -->        <!-- Placed at the end of the document so the pages load faster -->        <script src="js/bootstrap.min.js"></script>        <!-- Additional tags here -->        <?php if (function_exists('customPageFooter')){            customPageFooter();        }        ?>    </body></html>
